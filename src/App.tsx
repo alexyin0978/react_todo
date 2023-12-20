f375dce7eb3e08cf3cfb21ab4a88dc682ece14cf
Alex Yin <yinchihkai0909@gmail.com>
2023-12-20 12:24:23 +0800

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,119 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+type Todo = {
+  id: number;
+  name: string;
+  child?: Todo[];
+};
+
+const todoList: Todo[] = [
+  {
+    id: 1,
+    name: "food",
+    child: [
+      {
+        id: 1,
+        name: "burger",
+      },
+      {
+        id: 2,
+        name: "noodle",
+      },
+      {
+        id: 3,
+        name: "pizza",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "fruit",
+    child: [
+      {
+        id: 1,
+        name: "apple",
+      },
+      {
+        id: 2,
+        name: "grape",
+      },
+      {
+        id: 3,
+        name: "watermelon",
+      },
+    ],
+  },
+];
+
+/*
+process:
+1. create ParentTodo, add isChecked state to control the checkbox
+2. map todoList and render ParentTodo inside App 
+
+3. create ChildTodo, add isChecked state to control the checkbox
+4. map childTodoList and render ChildTodo inside ParentTodo
+
+5. pass isParentChecked as key and prop to ChildTodo in order to control childTodo state by parent
+*/
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      Answer here:
+      {todoList.map((todo) => {
+        return <ParentCheckbox key={todo.id} todo={todo} />;
+      })}
+    </div>
+  );
 }
 
-export default App
+const ParentCheckbox = ({ todo }: { todo: Todo }) => {
+  const [isChecked, setIsChecked] = useState(false);
+  return (
+    <div>
+      <label style={{ color: "yellow" }}>
+        {todo.name}:
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={() => setIsChecked(!isChecked)}
+        />
+      </label>
+      {todo.child &&
+        todo.child.map((childTodo) => {
+          return (
+            <ChildTodo
+              key={`${childTodo.id}${isChecked}`}
+              childTodo={childTodo}
+              isParentChecked={isChecked}
+            />
+          );
+        })}
+    </div>
+  );
+};
+
+const ChildTodo = ({
+  childTodo,
+  isParentChecked,
+}: {
+  childTodo: Todo;
+  isParentChecked: boolean;
+}) => {
+  const [isChecked, setIsChecked] = useState(isParentChecked);
+  return (
+    <div>
+      <label>
+        {childTodo.name}:
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={() => setIsChecked(!isChecked)}
+        />
+      </label>
+    </div>
+  );
+};
+
+export default App;
